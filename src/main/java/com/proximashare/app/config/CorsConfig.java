d1063d83cc6fe2552a542cc141a0ff55b7e65b89
g-WSL <gsakore22@gmail.com>
2025-08-22 18:00:44 +0530

    cors allowed links

--- src/main/java/com/proximashare/app/config/CorsConfig.java
+++ src/main/java/com/proximashare/app/config/CorsConfig.java
@@ -8,19 +8,21 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig {
 
-  @Bean
-  public WebMvcConfigurer corsConfigurer() {
-    return new WebMvcConfigurer() {
-      @SuppressWarnings("null")
-      @Override
-      public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-            .allowedOrigins("http://localhost:5173", "https://proximacloud.ddns.net")
-            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-            .allowedHeaders("*")
-            .allowCredentials(true)
-            .maxAge(3600);
-      }
-    };
-  }
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @SuppressWarnings("null")
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("https://proximacloud.in",
+                                        "https://www.proximacloud.in",
+                                        "https://share.proximacloud.in",
+                                        "https://share-proxima.vercel.app")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
 }
